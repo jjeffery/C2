f52c05535323a7169abb0492a603f3d152702db4
= <=>
2012-08-07 12:40:44 +1000

    Modify build scripts to build assemblies for .NET 4.0 and .NET 3.5

--- src/C2.Facilities.NH/Properties/AssemblyInfo.cs
+++ src/C2.Facilities.NH/Properties/AssemblyInfo.cs
@@ -2,7 +2,14 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Simple NHibernate Facility for Castle Windsor")]
+#if NET40
+[assembly: AssemblyTitle("NET40: Simple NHibernate Facility for Castle Windsor")]
+#endif
+
+#if NET35
+[assembly: AssemblyTitle("NET35: Simple NHibernate Facility for Castle Windsor")]
+#endif
+
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("4fa44623-9be7-4146-a723-3a8e86bcfab9")]
